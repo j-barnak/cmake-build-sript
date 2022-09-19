a5323d8974c134ce024ce3c589b47842a5ef8a8c
j-barnak <96548573+j-barnak@users.noreply.github.com>
2022-09-18 20:21:33 -0400

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+if [ "$1"  = "debug" ]
+then
+    if [ ! -d "debug" ]; then
+        mkdir debug
+    fi
+
+    cmake -B debug -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 . -GNinja
+    cd debug/
+    ninja
+    cd -
+
+elif [ "$1"  = "release" ]
+then
+    if [ ! -d "release" ]; then
+        mkdir release
+    fi
+
+    cmake -B release -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=1 . -GNinja
+    cd release/
+    ninja
+    mv ./glop ../
+    cd -
+
+else
+    echo "To build in debug:  ./build debug"
+    echo "To build in relase: ./build release"
+fi
